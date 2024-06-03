1e3bbbf817c81874a37f5befe2b7f989821f7e9d
Maharavo1 <hei.maharavo@gmail.com>
2024-06-03 19:32:25 +0300

    feat: Restaurant Unit

--- src/main/java/Projet1/KFC/repository/UnitCrudOperations.java
+++ src/main/java/Projet1/KFC/repository/UnitCrudOperations.java
@@ -1,4 +1,188 @@
 package Projet1.KFC.repository;
 
-public class UnitCrudOperations {
+import Projet1.KFC.db.DBConnection;
+import Projet1.KFC.model.Unit;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UnitCrudOperations implements CrudOperations <Unit> {
+    @Override
+    public Unit findById(int id) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        Unit unit = null;
+
+        try {
+            String sql = "SELECT * FROM unit WHERE unit_id = ?";
+            connection = DBConnection.getConnection();
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, id);
+            resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                unit = new Unit(
+                        resultSet.getInt("unit_id"),
+                        resultSet.getString("unit_name")
+                );
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        return unit;
+    }
+
+    @Override
+    public List<Unit> findAll() {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        List<Unit> units = new ArrayList<>();
+
+        try {
+            String sql = "SELECT * FROM unit";
+            connection = DBConnection.getConnection();
+            preparedStatement = connection.prepareStatement(sql);
+            resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                Unit unit = new Unit(
+                        resultSet.getInt("unit_id"),
+                        resultSet.getString("unit_name")
+                );
+                units.add(unit);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        return units;
+    }
+
+    @Override
+    public Unit save(Unit toSave) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+
+        try {
+            String sql = "INSERT INTO unit (unit_id, unit_name) VALUES (?, ?)";
+            connection = DBConnection.getConnection();
+            preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setInt(1, toSave.getUnitId());
+            preparedStatement.setString(2, toSave.getUnitName());
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        return toSave;
+    }
+
+    @Override
+    public Unit Update(Unit toUpdate) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+
+        try {
+            String sql = "UPDATE unit SET unit_name = ? WHERE unit_id = ?";
+            connection = DBConnection.getConnection();
+            preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setString(1, toUpdate.getUnitName());
+            preparedStatement.setInt(2, toUpdate.getUnitId());
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        return toUpdate;
+    }
+
+    @Override
+    public void delete(int id) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+
+        try {
+            String sql = "DELETE FROM unit WHERE unit_id = ?";
+            connection = DBConnection.getConnection();
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, id);
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
 }
