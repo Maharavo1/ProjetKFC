5bf89b3517bb42b67f34f6be9f81c3c4803116f7
Maharavo1 <hei.maharavo@gmail.com>
2024-06-03 19:19:21 +0300

    feat: Save Menu

--- src/main/java/Projet1/KFC/repository/MenuCrudOperations.java
+++ src/main/java/Projet1/KFC/repository/MenuCrudOperations.java
@@ -1,4 +1,192 @@
 package Projet1.KFC.repository;
 
-public class MenuCrudOperations {
+import Projet1.KFC.db.DBConnection;
+import Projet1.KFC.model.Menu;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MenuCrudOperations implements CrudOperations <Menu>{
+    @Override
+    public Menu findById(int id) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        Menu menu = null;
+
+        try {
+            String sql = "SELECT * FROM menu WHERE menu_id = ?";
+            connection = DBConnection.getConnection();
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, id);
+            resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                menu = new Menu(
+                        resultSet.getInt("menu_id"),
+                        resultSet.getString("menu_name"),
+                        resultSet.getDouble("price_sales")
+                );
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        return menu;
+    }
+
+    @Override
+    public List<Menu> findAll() {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        List<Menu> menus = new ArrayList<>();
+
+        try {
+            String sql = "SELECT * FROM menu";
+            connection = DBConnection.getConnection();
+            preparedStatement = connection.prepareStatement(sql);
+            resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                Menu menu = new Menu(
+                        resultSet.getInt("menu_id"),
+                        resultSet.getString("menu_name"),
+                        resultSet.getDouble("price_sales")
+                );
+                menus.add(menu);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        return menus;
+    }
+
+    @Override
+    public Menu save(Menu toSave) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+
+        try {
+            String sql = "INSERT INTO menu (menu_id, menu_name, price_sales) VALUES (?, ?, ?)";
+            connection = DBConnection.getConnection();
+            preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setInt(1, toSave.getMenuId());
+            preparedStatement.setString(2, toSave.getMenuName());
+            preparedStatement.setDouble(3, toSave.getPriceSales());
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        return toSave;
+    }
+
+    @Override
+    public Menu Update(Menu toUpdate) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+
+        try {
+            String sql = "UPDATE menu SET menu_name = ?, price_sales = ? WHERE menu_id = ?";
+            connection = DBConnection.getConnection();
+            preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setString(1, toUpdate.getMenuName());
+            preparedStatement.setDouble(2, toUpdate.getPriceSales());
+            preparedStatement.setInt(3, toUpdate.getMenuId());
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        return toUpdate;
+    }
+
+    @Override
+    public void delete(int id) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+
+        try {
+            String sql = "DELETE FROM menu WHERE menu_id = ?";
+            connection = DBConnection.getConnection();
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, id);
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
 }
